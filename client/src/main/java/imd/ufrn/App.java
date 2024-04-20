cbd445a13e7163e418111b8a3ab391759392e84c
anamgholandadev <ana.mgholandadev@gmail.com>
2024-04-20 16:22:03 -0300

    Small fix

--- client/src/main/java/imd/ufrn/App.java
+++ client/src/main/java/imd/ufrn/App.java
@@ -20,32 +20,38 @@ public class App {
                 String choice = reader.readLine();
                 switch (choice) {
                     case "1":
+                        System.out.print("Enter id: ");
+                        String id = reader.readLine();
                         System.out.print("Enter name: ");
                         String name = reader.readLine();
                         System.out.print("Enter color: ");
                         String color = reader.readLine();
                         System.out.print("Enter owner: ");
                         String owner = reader.readLine();
+                        String parameters = id + ";" + name + ";" + color + ";" + owner;
                         // Call stub of createCat passing the parameters (name, color, owner);
                         break;
                     case "2":
+                        System.out.print("Enter id (for update): ");
+                        String idUpdate = reader.readLine();
                         System.out.print("Enter name (for update): ");
                         String nameUpdate = reader.readLine();
                         System.out.print("Enter color (for update): ");
                         String colorUpdate = reader.readLine();
                         System.out.print("Enter owner (for update): ");
                         String ownerUpdate = reader.readLine();
+                        String parametersUpdate = idUpdate + ";" + nameUpdate + ";" + colorUpdate + ";" + ownerUpdate;
                         // Call stub of updateCat passing the parameters (nameUpdate, colorUpdate,
                         // ownerUpdate);
                         break;
                     case "3":
-                        System.out.print("Enter name (to read): ");
-                        String nameRead = reader.readLine();
-                        // Call stub of readCat passing the parameter(nameRead);
+                        System.out.print("Enter id (to read): ");
+                        String idRead = reader.readLine();
+                        // Call stub of readCat passing the parameter(idRead);
                     case "4":
-                        System.out.print("Enter name (to delete): ");
-                        String nameDelete = reader.readLine();
-                        // Call stub of deleteCat passing the parameter(nameRead);
+                        System.out.print("Enter id (to delete): ");
+                        String idDelete = reader.readLine();
+                        // Call stub of deleteCat passing the parameter(idDelete);
                         break;
                     case "5":
                         // stub.close();
